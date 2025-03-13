9a74186a8bea1bde2df91ec76f4fc25b70f9126c
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-03-13 06:09:00 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -1,18 +1,20 @@
 package com.javatechie;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
 public class DevopsIntegrationApplication {
 
     @GetMapping("/")
-    public String welcome()yuy {
+    public String welcome() {
         return "welcome"; // Thymeleaf will look for welcome.html in the templates folder
     }
 
     public static void main(String[] args) {
-        rtrSpringApplication.run(DevopsIntegrationApplication.class, args);
-        rtrSpringApplication.run(DevopsIntegrationApplication.class, args);
+        SpringApplication.run(DevopsIntegrationApplication.class, args);
     }
 }
