08a12b53d594d1fcd4b825211b204232b2c63a86
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-01-09 14:35:35 +0530

    Create UserRepository.java

---
+++ src/test/java/com/javatechie/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.javatechie.repository;
+
+import com.javatechie.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
