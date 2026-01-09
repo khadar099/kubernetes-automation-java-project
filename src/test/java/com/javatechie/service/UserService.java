5dfe25b9f84a5b1a32879470bc80537d6308bb0e
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-01-09 14:36:30 +0530

    Create UserService.java

---
+++ src/test/java/com/javatechie/service/UserService.java
@@ -0,0 +1,19 @@
+package com.javatechie.service;
+
+import com.javatechie.entity.User;
+import com.javatechie.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+}
