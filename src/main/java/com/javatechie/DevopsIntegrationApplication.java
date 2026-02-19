af4f02b053a1ad0968576143ab72e5924fca4ae0
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-02-19 16:03:32 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -2,19 +2,15 @@ package com.javatechie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
-@Controller
-public class DevopsIntegrationApplication {
+public class DemoService {
 
-    @GetMapping("/")
-    public String welcome() {
-        return "welcome"; // Thymeleaf will look for welcome.html in the templates folder
+    public void test() {
+        String a = "hello";
+        String b = "hello";   // duplicate literal (code smell)
+
+        if(true == true) {    // bad practice
+            System.out.println("Always true");
+        }
     }
-
-    public static void main(String[] args) {
-        SpringApplication.run(DevopsIntegrationApplication.class, args);
-    }
-}
+    ]
